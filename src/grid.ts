f4f3695c1949915c639649e78e9bc9c216e0e441
Light-JH <phoebehu@outlook.com>
2023-07-26 22:25:29 -0700

    update

--- src/grid.ts
+++ src/grid.ts
@@ -1,6 +1,3 @@
-
-
-
 export function CreateMatrix(rows: number, columns: number): number[][] {
     const matrix: number[][] = [];
     for (let i = 0; i < rows; i++) {
@@ -13,23 +10,8 @@ export function CreateMatrix(rows: number, columns: number): number[][] {
     return matrix;
 }
 
-
-const rows: number = parseInt(process.argv[2], 10);
-const columns: number = parseInt(process.argv[3], 10);
-
-if (isNaN(rows) || isNaN(columns)) {
-    console.error("Please provide valid rows and columns numbers.");
-    process.exit(1);
-}
-
-let matrix = CreateMatrix(rows, columns);
-
-// print matrix.
-
 export function printMatrix(matrix: number[][]): void {
     for (const row of matrix) {
         console.log(row.join('\t'))
     }
 }
-
-printMatrix(matrix);
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
 import { CreateMatrix } from "./grid";
 import { printMatrix } from "./grid";
-import { updateGrid } from "./game";
+import { updateCell } from "./game";
 
+const rows: number = parseInt(process.argv[2], 10);
+const columns: number = parseInt(process.argv[3], 10);
+console.log("help", rows, columns);
+if (isNaN(rows) || isNaN(columns)) {
+    console.error("Please provide valid rows and columns numbers.");
+    process.exit(1);
+}
+
+const matrix = CreateMatrix(rows, columns);
+
+// print matrix.
+printMatrix(matrix);
