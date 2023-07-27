75fb5f963ae4e5a94a374c78b41354f1af0e2b19
Phoebe Hu <68739569+Light-JH@users.noreply.github.com>
2023-07-27 11:52:37 -0700

    2 implementing the rules of life (#4)
    * update matrix with random inital value
    
    * add game and main
    
    * remove node modules, fix gitignore
    
    * update
    
    * fix matrix issue

---
+++ src/game.ts
@@ -0,0 +1,60 @@
+// Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+// Any live cell with two or three live neighbours lives on to the next generation.
+// Any live cell with more than three live neighbours dies, as if by overpopulation.
+// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+import { CreateMatrix, printMatrix } from "./grid";
+
+export function updateMatrix(matrix: number[][]) {
+    let neighborMatrix = matrixNeighbors(matrix);
+    console.log("live map");
+    printMatrix(neighborMatrix);
+    let rows = neighborMatrix.length
+    let cols = neighborMatrix[0].length
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            let live = neighborMatrix[r][c];
+            let cur = matrix[r][c];
+            if (cur === 1) {
+                if (live < 2 || live > 3) {
+                    matrix[r][c] = 0;
+                }
+            } else if (live === 3) {
+                matrix[r][c] = 1;
+            }
+        }
+    }
+
+    function numofLiveNeighbors(matrix: number[][], row: number, col: number): number {
+        let rows = [row - 1, row, row + 1]
+        let cols = [col - 1, col, col + 1]
+        let live = 0;
+        for (var r of rows) {
+            for (var c of cols) {
+                if (r >= 0 && r < matrix.length && c >= 0 && c < matrix[0].length) {
+                    if (matrix[r][c] === 1) {
+                        live += 1;
+                    }
+                }
+            }
+        }
+        // return number of live neighbors
+        if (matrix[row][col] === 1) {
+            return live - 1;
+        } else {
+            return live;
+        }
+    }
+
+    function matrixNeighbors(matrix: number[][]): number[][] {
+        let rows = matrix.length;
+        let cols = matrix[0].length;
+        let liveMatrix = CreateMatrix(rows, cols);
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < cols; c++) {
+                liveMatrix[r][c] = numofLiveNeighbors(matrix, r, c);
+            }
+        }
+        return liveMatrix;
+    }
+}
--- src/grid.ts
+++ src/grid.ts
@@ -1,29 +1,17 @@
-
-function CreateMatrix(rows: number, columns: number): number[][] {
+export function CreateMatrix(rows: number, columns: number): number[][] {
     const matrix: number[][] = [];
     for (let i = 0; i < rows; i++) {
-        matrix.push(new Array(columns).fill(0));
+        const row: number[] = [];
+        for (let j = 0; j < columns; j++) {
+            row.push(Math.round(Math.random()));
+        }
+        matrix.push(row);
     }
     return matrix;
 }
 
-
-const rows: number = parseInt(process.argv[2], 10);
-const columns: number = parseInt(process.argv[3], 10);
-
-if (isNaN(rows) || isNaN(columns)) {
-    console.error("Please provide valid rows and columns numbers.");
-    process.exit(1);
-}
-
-let matrix = CreateMatrix(rows, columns);
-
-// print matrix.
-
-function printMatrix(matrix: number[][]): void {
+export function printMatrix(matrix: number[][]): void {
     for (const row of matrix) {
         console.log(row.join('\t'))
     }
 }
-
-printMatrix(matrix);
